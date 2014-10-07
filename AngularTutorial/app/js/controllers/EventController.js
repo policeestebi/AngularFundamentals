958bce39145539f3bf8ebd58cebcfe5f6f542f94
Esteban Ramírez González <policeestebigmail.com>
2014-10-07 15:03:03 -0700

    Add content

--- AngularTutorial/app/js/controllers/EventController.js
+++ AngularTutorial/app/js/controllers/EventController.js
@@ -21,24 +21,36 @@ eventsApp.controller('EventController',
                     creatorName: 'Bob Smith',
                     duration: '1 hr',
                     level: 'Advanced',
-                    abstract: 'In this session you will learn the ins and outs of directives!'
+                    abstract: 'In this session you will learn the ins and outs of directives!',
+                    upVoteCount: 0
                 },
                 {
                     name: 'Scopes for fun and profit',
                     creatorName: 'Jogn Doe',
                     duration: '30 mins',
                     level: 'Introductory',
-                    abstract: 'This session will take a closer look at scopes.'
+                    abstract: 'This session will take a closer look at scopes.',
+                    upVoteCount: 0
                 },
                 {
                     name: 'Well Behaved Controllers',
                     creatorName: 'Jane Doe',
                     duration: '2 hours',
                     level: 'Intermediate',
-                    abstract: 'Controller are the beginning of everything.'
+                    abstract: 'Controller are the beginning of everything.',
+                    upVoteCount: 0
                 }
             ]
         }
+
+        $scope.upVoteSession = function (session) {
+
+            session.upVoteCount++;
+        };
+
+        $scope.downVoteSession = function (session) {
+            session.upVoteCount--;
+        };
 
     }
 
