20ae9df736881b716fe295e0d6fa2b9ba87c232f
Esteban Ramírez González <policeestebigmail.com>
2014-10-07 14:41:19 -0700

    Initial Commit

---
+++ AngularTutorial/app/js/controllers/EventController.js
@@ -0,0 +1,45 @@
+﻿'use strict';
+
+eventsApp.controller('EventController',
+
+    function EvenController($scope) {
+
+        $scope.event = {
+
+            name: "Angular Boot Camp",
+            date: '1/10/2014',
+            time: '10:30 am',
+            location: {
+                address: 'Google Headquarters',
+                city: 'Mountain View',
+                province: 'CA'
+            },
+            imageUrl: 'img/angularjs-logo.jpg',
+            sessions: [
+                {
+                    name: 'Directives Masterclass',
+                    creatorName: 'Bob Smith',
+                    duration: '1 hr',
+                    level: 'Advanced',
+                    abstract: 'In this session you will learn the ins and outs of directives!'
+                },
+                {
+                    name: 'Scopes for fun and profit',
+                    creatorName: 'Jogn Doe',
+                    duration: '30 mins',
+                    level: 'Introductory',
+                    abstract: 'This session will take a closer look at scopes.'
+                },
+                {
+                    name: 'Well Behaved Controllers',
+                    creatorName: 'Jane Doe',
+                    duration: '2 hours',
+                    level: 'Intermediate',
+                    abstract: 'Controller are the beginning of everything.'
+                }
+            ]
+        }
+
+    }
+
+);
---
+++ AngularTutorial/obj/Release/Package/PackageTmp/app/js/controllers/EventController.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+eventsApp.controller('EventController',
+
+    function EvenController($scope) {
+
+        $scope.event = {
+
+            name: "Angular Boot Camp",
+            date: '1/10/2014',
+            time: '10:30 am'
+
+        }
+
+    }
+
+);
