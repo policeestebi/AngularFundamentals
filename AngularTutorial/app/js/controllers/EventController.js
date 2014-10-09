42f5bdd7a31fd3c197807242b1d3084c2d33a09f
Esteban Ramírez González <policeestebigmail.com>
2014-10-09 15:47:22 -0700

    Custom Services

--- AngularTutorial/app/js/controllers/EventController.js
+++ AngularTutorial/app/js/controllers/EventController.js
@@ -2,7 +2,7 @@
 
 eventsApp.controller('EventController',
 
-    function EvenController($scope) {
+    function EvenController($scope, eventData) {
 
         $scope.myStyle = {color:'red'};
 
@@ -16,44 +16,7 @@ eventsApp.controller('EventController',
 
         $scope.sortorder = 'name';
 
-        $scope.event = {
-
-            name: "Angular Boot Camp",
-            date: 1359781015626,
-            time: '10:30 am',
-            location: {
-                address: 'Google Headquarters',
-                city: 'Mountain View',
-                province: 'CA'
-            },
-            imageUrl: 'img/angularjs-logo.jpg',
-            sessions: [
-                {
-                    name: 'Directives Masterclass',
-                    creatorName: 'Bob Smith',
-                    duration: 1,
-                    level: 'Advanced',
-                    abstract: 'In this session you will learn the ins and outs of directives!',
-                    upVoteCount: 0
-                },
-                {
-                    name: 'Scopes for fun and profit',
-                    creatorName: 'Jogn Doe',
-                    duration: 3,
-                    level: 'Introductory',
-                    abstract: 'This session will take a closer look at scopes.',
-                    upVoteCount: 0
-                },
-                {
-                    name: 'Well Behaved Controllers',
-                    creatorName: 'Jane Doe',
-                    duration: 2,
-                    level: 'Intermediate',
-                    abstract: 'Controller are the beginning of everything.',
-                    upVoteCount: 0
-                }
-            ]
-        };
+        $scope.event = eventData.event;
 
         $scope.upVoteSession = function (session) {
 
--- AngularTutorial/app/js/services/eventData.js
+++ AngularTutorial/app/js/services/eventData.js
@@ -1,48 +1,45 @@
 'use strict';
 
-eventsApp.factory('eventData', function (eventResource, authService) {
+eventsApp.factory('eventData', function ( ) {
     return {
-        getEvent: function(eventId, callback) {
-            return eventResource.get({id:eventId}, function(event) {
-                if (callback)
-                    callback(event);
-            });
-        },
-        getAllEvents: function(callback) {
-            return eventResource.queryAll(callback);
-        },
-        getNextSessionId:function (event) {
-            var max = 0;
-            for (var idx = 0; idx < event.sessions.length; idx++) {
-                if (event.sessions[idx].id > max) {
-                    max = event.sessions[idx].id;
-                }
-            }
-            return max+1;
-        },
-        save: function(event, callback) {
-            if (event.id) {
-                eventResource.save(event, function() { if (callback) callback(); });
-            } else {
-                eventResource.queryAll(function(events) {
-                    event.creator = authService.getCurrentUserName();
-                    event.id = getNextEventId(events);
-                    event.sessions = [];
-                    eventResource.save(event);
-                    if (callback)
-                        callback();
-                });
-            }
-        }
-    };
+        event: {
 
-    function getNextEventId(events) {
-        var max = 0;
-        for (var idx = 0; idx < events.length; idx++) {
-            if (events[idx].id > max) {
-                max = events[idx].id;
-            }
+            name: "Angular Boot Camp",
+            date: 1359781015626,
+            time: '10:30 am',
+            location: {
+                address: 'Google Headquarters',
+                city: 'Mountain View',
+                province: 'CA'
+            },
+            imageUrl: 'img/angularjs-logo.jpg',
+            sessions: [
+                {
+                    name: 'Directives Masterclass',
+                    creatorName: 'Bob Smith',
+                    duration: 1,
+                    level: 'Advanced',
+                    abstract: 'In this session you will learn the ins and outs of directives!',
+                    upVoteCount: 0
+                },
+                {
+                    name: 'Scopes for fun and profit',
+                    creatorName: 'Jogn Doe',
+                    duration: 3,
+                    level: 'Introductory',
+                    abstract: 'This session will take a closer look at scopes.',
+                    upVoteCount: 0
+                },
+                {
+                    name: 'Well Behaved Controllers',
+                    creatorName: 'Jane Doe',
+                    duration: 2,
+                    level: 'Intermediate',
+                    abstract: 'Controller are the beginning of everything.',
+                    upVoteCount: 0
+                }
+            ]
         }
-        return max+1;
-    }
+
+    };
 });
